338b64bfd93a7b1bb79d0e3af23ecc03b98db612
catsital <catshital@gmail.com>
2021-10-19 19:39:50 +0800

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setup(
     name="pyccoma",
-    version="0.2.0",
+    version="0.2.1",
     author="catsital",
     author_email="catshital@gmail.com",
     description="Scrape and download from Piccoma.",
